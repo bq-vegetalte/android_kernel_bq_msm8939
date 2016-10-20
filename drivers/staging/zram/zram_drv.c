0fb2f9c076b7147355abc4d48b4cc9e200374b39
paloda <paloda09@gmail.com>
2016-10-20 08:19:08 +0200

    Add more Zram
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>
    (cherry picked from commit 407bd59c97f0590fe5e2443025fcdb531c74ac6b)
    (cherry picked from commit 603a2c5a71d5368f6c6e732dcbe7ac0693dd968a) (reverted from commit 77df7d3e5d80d10c2061c0b41f628baf9751fb12)

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -49,7 +49,7 @@ static const char *default_compressor = "lz4";
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 4;
+static unsigned int num_devices = 1;
 
 static int zram_show_mem_notifier(struct notifier_block *nb,
 				unsigned long action,
